de8e3eed3b5ddb043805d15f63b89a6b3d4ddf51
diegomunizdev <diegomunizdev@outlook.com>
2022-07-11 16:35:14 -0300

    update

--- src/infrastructure/repositories/account/account.repository.spec.ts
+++ src/infrastructure/repositories/account/account.repository.spec.ts
@@ -1,25 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 
-import { Account } from '../../../infrastructure/entities/account/account.entity';
 import { AccountRepository } from './account.repository';
 import {
   mockAccount,
   mockAccountsRepository,
 } from '../../../domain/mocks/account/account.mock';
-import { connectionDatabase } from '../../../infrastructure/config/database/database';
 
 describe('AccountRepository', () => {
   let accountRepository: AccountRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forFeature([Account]),
-        TypeOrmModule.forRoot(connectionDatabase),
-      ],
-      providers: [AccountRepository, Repository],
+      providers: [AccountRepository],
     }).compile();
 
     accountRepository = module.get<AccountRepository>(AccountRepository);
